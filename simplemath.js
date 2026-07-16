ac2fc8ba173bfa72ea2c537a31d276241c8ec7b2
sbshaon007 <sbshaon21@gmail.com>
2026-07-17 02:22:48 +0600

    reached task data type indentification

---
+++ simplemath.js
@@ -0,0 +1,26 @@
+let num1 = 25;
+
+let num2 = 10;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+console.log('Addition: ' + addition);
+console.log(typeof addition);
+
+console.log('Subtraction: ' + subtraction);
+console.log(typeof subtraction);
+
+console.log('Multiplication: ' + multiplication);
+console.log(typeof multiplication);
+
+console.log('Division: ' + division);
+console.log(typeof division);
+
+console.log('Modulus: ' + modulus);
+console.log(typeof modulus);
+
+ 
