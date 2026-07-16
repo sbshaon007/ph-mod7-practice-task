bd6f4530ed4d4272e3dba472a106ca9ee5f41b56
sbshaon007 <sbshaon21@gmail.com>
2026-07-17 03:24:59 +0600

    Mini coding challenge

---
+++ challenge.js
@@ -0,0 +1,18 @@
+let price = 200;
+let quantity = 3;
+let discount = 10; // percent
+
+let totalPrice = price * quantity;
+let discountAmount = (totalPrice*discount)/100;
+let finalPrice = totalPrice - discountAmount;
+
+console.log('Total Price: ' , totalPrice);
+console.log('Discount Amount: ' ,discountAmount);
+console.log('Final Price: ' , finalPrice);
+
+
+// Expected Console Output
+
+// Total Price: 600
+// Discount Amount: 60
+// Final Price: 540
