40a8bbca6d4b6aa86ba640ca32709791a92ce565
sbshaon007 <sbshaon21@gmail.com>
2026-07-17 03:12:28 +0600

    shorthand practice task

---
+++ shorthand.js
@@ -0,0 +1,29 @@
+let score = 50;
+
+score += 10;
+console.log('After += 10: ' , score);
+
+score -= 5;
+console.log('After -= 5: ', score);
+
+score *= 2;
+console.log('After *= 2:' , score);
+
+score /= 5;
+console.log('After /= 5: ' , score);
+
+score %= 4;
+console.log('After %= 4: ' , score);
+
+
+// Expected Console Output
+
+// After += 10: 60
+
+// After -= 5: 55
+
+// After *= 2: 110
+
+// After /= 5: 22
+
+// After %= 4: 2
