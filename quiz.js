07e21be5b0cc16993075a795f2e2035c695de137
sbshaon007 <sbshaon21@gmail.com>
2026-07-17 03:28:53 +0600

    console quiz

---
+++ quiz.js
@@ -0,0 +1,29 @@
+// Question 1
+
+// let x = 20; let y = 5; console.log(x + y);
+
+// Answer: 25
+
+// Question 2
+
+// let language = "JavaScript"; console.log(typeof language);
+
+// Answer: string
+
+// Question 3
+
+// let isOnline = false; console.log(typeof isOnline);
+
+// Answer: boolean
+
+// Question 4
+
+// let num = 10; num += 5; console.log(num);
+
+// Answer: 15
+
+// Question 5
+
+// let a = 30; let b = 7; console.log(a % b);
+
+// Answer: 2
