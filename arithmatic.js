70ca36c8e73a69f953139c1f8cc28e999049afd1
sbshaon007 <sbshaon21@gmail.com>
2026-07-17 03:02:03 +0600

    arithmatic practice task

---
+++ arithmatic.js
@@ -0,0 +1,21 @@
+let number1 = 50;
+let number2 = 8;
+
+
+console.log('Number 1:', number1);
+console.log('Number 2:', number2);
+console.log('Addition: ' , number1 + number2);
+console.log('Subtraction: ' , number1 - number2);
+console.log('Multiplication: ', number1 * number2);
+console.log('Division: ' , number1 / number2);
+console.log('Modulus: ' , number1 % number2);
+
+// Expected Console Output
+
+// Number 1: 50
+// Number 2: 8
+// Addition: 58
+// Subtraction: 42
+// Multiplication: 400
+// Division: 6.25
+// Modulus: 2
